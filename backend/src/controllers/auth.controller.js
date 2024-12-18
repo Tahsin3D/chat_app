d9eb80b8d66a64335b2629cdf8427660c06bb3c0
Tahsin Haider <tahsin3194@gmail.com>
2024-12-18 13:48:14 +0500

    testing signup post req

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,30 +1,47 @@
+import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
-import bcrypt from 'bcryptjs';
+import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
-  const {fullName, email, password} = req.body;
+  const { fullName, email, password } = req.body;
 
-  try{
-    if(password.length < 6)
-    {
-        return res.status(400).json({message: "Password must be 6 least character"});
+  try {
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be 6 least character" });
     }
 
-    const user = await User.findOne({email});
+    const user = await User.findOne({ email });
 
-    if(user) return res.status(400).json({message: "Email already exists."});
+    if (user) return res.status(400).json({ message: "Email already exists." });
 
     const salt = await bcrypt.genSalt(10);
-    const hashedPassword= await bcrypt.hash(password, salt);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     const newUser = new User({
-        fullName,
-        email,
-        password: hashedPassword
-    })
-  }
-  catch(error){
-    console.log(error)
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      //generate webtoken
+      generateToken(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ message: "Invalid data" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Missing fields" });
   }
 };
 
