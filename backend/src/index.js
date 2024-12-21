db5019f015922e19e20c1257fdab618becfbb918
Tahsin Haider <tahsin3194@gmail.com>
2024-12-21 18:38:46 +0500

    messages backend system

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import authRoutes from "./routes/auth.route.js";
 import dotenv from 'dotenv'
 import { connectDB } from "./lib/db.js";
 import cookieParser from 'cookie-parser'
+import messageRoutes from './routes/message.route.js'
 
 dotenv.config();
 
@@ -16,6 +17,7 @@ app.use(cookieParser())
 
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on port: "+ PORT);
---
+++ backend/src/models/message.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    senderId: {
+      required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    receiverId: {
+      required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    text: {
+      type: String,
+    },
+
+    image: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
