7e682f7097b1f0d22bee6500a9d8016c4094ca92
Tahsin Haider <tahsin3194@gmail.com>
2024-12-22 16:22:22 +0500

    set up zustand store for state management

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ import dotenv from 'dotenv'
 import { connectDB } from "./lib/db.js";
 import cookieParser from 'cookie-parser'
 import messageRoutes from './routes/message.route.js'
+import cors from 'cors';
 
 dotenv.config();
 
@@ -13,7 +14,13 @@ const PORT = process.env.PORT;
 
 // Add JSON parsing middleware
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
+app.use(cors(
+  {
+    origin: "http://localhost:5173",
+    credentials: true
+  }
+))
 
 
 app.use("/api/auth", authRoutes);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,40 @@
-import {Routes, Route} from 'react-router-dom';
-import HomePage from './pages/HomePage.jsx'
-import SignUpPage from './pages/SignUpPage.jsx'
-import LoginPage from './pages/LoginPage.jsx'
-import SettingsPage from './pages/SettingsPage.jsx'
-import ProfilePage from './pages/ProfilePage.jsx'
-import Navbar from './components/Navbar.jsx'
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SettingsPage from "./pages/SettingsPage.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
+import Navbar from "./components/Navbar.jsx";
+import { useAuthStore } from "./store/useAuthStore.js";
+import { useEffect } from "react";
+import { Box, CircularProgress } from "@mui/material";
 
 const App = () => {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
 
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+
+  if (isCheckingAuth && !authUser)
+    return (
+      <Box sx={{width: '100%', display: 'flex', height: '100vh',}}>
+        <CircularProgress sx={{margin: "auto"}}/>
+      </Box>
+    );
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/signup" element={<SignUpPage/>}/>
-        <Route path="/login" element={<LoginPage/>}/>
-        <Route path="/setting" element={<SettingsPage/>}/>
-        <Route path="/profile" element={<ProfilePage/>}/>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/setting" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
