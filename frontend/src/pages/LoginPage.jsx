90ad69390ad91d4c091470f1096b4279f76d987b
Tahsin Haider <tahsin3194@gmail.com>
2024-12-24 14:16:24 +0500

    signin signup responsiveness setup

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -28,163 +28,82 @@ const LoginPage = () => {
     e.preventDefault();
   };
   return (
-    <Box sx={{ display: "flex", height: "100vh", alignItems: "center" }}>
-      <Grid
-        container
+    <Box
+      sx={{
+        width: "100vw",
+        backgroundColor: "white",
+        display: "flex",
+        height: { xs: "calc(100vh-50px)", md: "100vh" },
+      }}
+    >
+      <Box
         sx={{
-          height: "calc(100vh - 20px)",
-          // backgroundColor: "red",
-          // display: "flex",
+          display: "flex",
+          height: "100%",
           alignItems: "center",
-          // justifyContent: "center"
+          width: "100%",
+          maxWidth: "1920px",
+          margin: "0 auto",
+          backgroundColor: "white",
         }}
       >
-        <Grid item xs={12} md={6}>
-          <Box
-            sx={{
-              backgroundColor: color,
-              height: "100%",
-              borderRadius: "0 5% 5% 0 ",
-              display: { md: "flex", xs: "none" },
-            }}
+        <Grid
+          container
+          sx={{
+            height: "calc(100% - 20px)",
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          {/* Other Page Link Side */}
+          <Grid
+            item
+            xs={12}
+            md={6}
+            sx={{ display: { md: "flex", xs: "none" }, width: "100%" }}
           >
             <Box
               sx={{
-                color: "white",
-                padding: "70px",
-                textAlign: "center",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
+                backgroundColor: color,
+                width: "100%",
                 height: "100%",
-                maxWidth: "550px",
+                borderRadius: "0 5% 5% 0 ",
+                display: { md: "flex", xs: "none" },
+                justifyContent: "flex-start",
               }}
             >
-              <Typography
-                variant="h2"
-                textAlign="center"
-                fontSize="2.5rem"
-                fontWeight="bold"
-              >
-                Don't have an account?
-              </Typography>
-              <Typography mt="20px" variant="body2" fontSize="1.2rem">
-                Make a new account to get connected to your friends
-              </Typography>
-              <Button
-                component={Link}
-                to="/signup"
+              <Box
                 sx={{
-                  backgroundColor: color,
                   color: "white",
-                  border: "1px solid white",
-                  width: "200px",
-                  padding: "10px 20px",
-                  fontSize: "1.2rem",
-                  borderRadius: "10px",
-                  mt: "50px",
+                  padding: "70px",
+                  textAlign: "center",
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  height: "100%",
+                  maxWidth: "550px",
                 }}
               >
-                SIGN UP
-              </Button>
-            </Box>
-          </Box>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Box
-            sx={{
-              height: "100%",
-              padding: { md: "0", lg: "0 70px" },
-              display: "flex",
-              alignItems: "center",
-              justifyContent: { lg: "flex-end", xs: "center" },
-            }}
-          >
-            <Box
-              sx={{
-                textAlign: "center",
-                padding: "20px",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                gap: "10px",
-                maxWidth: "450px",
-              }}
-            >
-              <Box>
-                <ChatBubbleOutlineIcon
-                  sx={{
-                    fontSize: "4rem",
-                    backgroundColor: color,
-                    color: "white",
-                    padding: "10px",
-                    borderRadius: "10px",
-                  }}
-                />
                 <Typography
-                  mb="10px"
-                  color={color}
                   variant="h2"
+                  textAlign="center"
                   fontSize="2.5rem"
                   fontWeight="bold"
                 >
-                  Sign In
+                  Don't have an account?
                 </Typography>
-                <Typography variant="body2" fontSize="1.2rem">
-                  Please verify yourself to go to your account
+                <Typography
+                  color="black"
+                  mt="50px"
+                  variant="body2"
+                  fontSize="1.2rem"
+                >
+                  Make a new account to get connected to your friends
                 </Typography>
-              </Box>
-              <form onSubmit={handleSubmit}>
-                {/* Email */}
-                <TextField
-                  label="Email"
-                  name="email"
-                  type="email"
-                  value={formData.email}
-                  onChange={(e) =>
-                    setFormData({ ...formData, email: e.target.value })
-                  }
-                  fullWidth
-                  margin="normal"
-                  required
-                />
-
-                {/* Password */}
-                <TextField
-                  label="Password"
-                  name="password"
-                  type={showPassword ? "text" : "password"}
-                  value={formData.password}
-                  onChange={(e) =>
-                    setFormData({ ...formData, password: e.target.value })
-                  }
-                  fullWidth
-                  margin="normal"
-                  required
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <IconButton
-                          onClick={() => setShowPassword(!showPassword)}
-                          edge="end"
-                          aria-label={
-                            showPassword ? "Hide password" : "Show password"
-                          }
-                        >
-                          {showPassword ? (
-                            <VisibilityOffIcon />
-                          ) : (
-                            <VisibilityIcon />
-                          )}
-                        </IconButton>
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-
-                {/* Signup Button */}
                 <Button
+                  component={Link}
+                  to="/signup"
                   sx={{
                     backgroundColor: color,
                     color: "white",
@@ -196,19 +115,133 @@ const LoginPage = () => {
                     mt: "50px",
                   }}
                 >
-                  LOGIN
+                  SIGN UP
                 </Button>
-              </form>
-              <Box sx={{ display: { xs: "flex", md: "none" } }}>
-                <Stack direction="row" gap="10px">
-                  <Typography variant="span">Don't have an account?</Typography>
-                  <Link to="/signup">SignUp</Link>
-                </Stack>
               </Box>
             </Box>
-          </Box>
+          </Grid>
+          {/* Form Side */}
+          <Grid item xs={12} md={6}>
+            <Box
+              sx={{
+                height: "100%",
+                padding: { xs: "40px 0", md: "0 70px" },
+                display: "flex",
+                alignItems: { md: "center", xs: "start" },
+                justifyContent: { lg: "flex-end", xs: "center" },
+              }}
+            >
+              <Box
+                sx={{
+                  textAlign: "center",
+                  padding: "20px",
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  gap: "40px",
+                  maxWidth: "450px",
+                }}
+              >
+                <Box>
+                  <ChatBubbleOutlineIcon
+                    sx={{
+                      fontSize: "4rem",
+                      backgroundColor: color,
+                      color: "white",
+                      padding: "10px",
+                      borderRadius: "10px",
+                    }}
+                  />
+                  <Typography
+                    mb="10px"
+                    color={color}
+                    variant="h2"
+                    fontSize="2.5rem"
+                    fontWeight="bold"
+                  >
+                    Sign In
+                  </Typography>
+                  <Typography color="gray" variant="body2" fontSize="1.2rem">
+                    Please verify yourself to go to your account
+                  </Typography>
+                </Box>
+                <form onSubmit={handleSubmit}>
+                  {/* Email */}
+                  <TextField
+                    label="Email"
+                    name="email"
+                    type="email"
+                    value={formData.email}
+                    onChange={(e) =>
+                      setFormData({ ...formData, email: e.target.value })
+                    }
+                    fullWidth
+                    margin="normal"
+                    required
+                  />
+
+                  {/* Password */}
+                  <TextField
+                    label="Password"
+                    name="password"
+                    type={showPassword ? "text" : "password"}
+                    value={formData.password}
+                    onChange={(e) =>
+                      setFormData({ ...formData, password: e.target.value })
+                    }
+                    fullWidth
+                    margin="normal"
+                    required
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <IconButton
+                            onClick={() => setShowPassword(!showPassword)}
+                            edge="end"
+                            aria-label={
+                              showPassword ? "Hide password" : "Show password"
+                            }
+                          >
+                            {showPassword ? (
+                              <VisibilityOffIcon />
+                            ) : (
+                              <VisibilityIcon />
+                            )}
+                          </IconButton>
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+
+                  {/* Signup Button */}
+                  <Button
+                    sx={{
+                      backgroundColor: color,
+                      color: "white",
+                      border: "1px solid white",
+                      width: "200px",
+                      padding: "10px 20px",
+                      fontSize: "1.2rem",
+                      borderRadius: "10px",
+                      mt: "50px",
+                    }}
+                  >
+                    LOGIN
+                  </Button>
+                </form>
+                <Box sx={{ display: { xs: "flex", md: "none" } }}>
+                  <Stack direction="row" gap="10px">
+                    <Typography variant="span">
+                      Don't have an account?
+                    </Typography>
+                    <Link to="/signup">SignUp</Link>
+                  </Stack>
+                </Box>
+              </Box>
+            </Box>
+          </Grid>
         </Grid>
-      </Grid>
+      </Box>
     </Box>
   );
 };
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -7,6 +7,7 @@ import {
   Grid,
   IconButton,
   InputAdornment,
+  Stack,
   TextField,
   Typography,
 } from "@mui/material";
@@ -28,113 +29,206 @@ const SignUpPage = () => {
     e.preventDefault();
   };
   return (
-    <Box sx={{ display: "flex", height: "100vh", alignItems: "center" }}>
-      <Grid container sx={{ height: "calc(100vh - 20px)" }}>
-        <Grid item xs={12} md={6}>
-          <Box
-            sx={{
-              height: "100%",
-              padding: "0 70px",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "flex-start"
-            }}
-          >
+    <Box sx={{
+      width: "100vw",
+      backgroundColor: "white",
+      display: "flex",
+      height: { xs: "calc(100vh-50px)", md: "100vh" },
+    }}>
+      <Box
+        sx={{
+          display: "flex",
+          height: "100%",
+          alignItems: "center",
+          width: "100%",
+          maxWidth: "1920px",
+          margin: "0 auto",
+          backgroundColor: "white",
+        }}
+      >
+        <Grid
+          container
+          sx={{
+            height: "calc(100% - 20px)",
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          {/* Form Side */}
+          <Grid item xs={12} md={6}>
             <Box
               sx={{
-                textAlign: "center",
-                padding: "20px",
+                height: "100%",
+                padding: { xs: "40px 0", md: "0 70px" },
                 display: "flex",
-                flexDirection: "column",
-                gap: "10px",
-                maxWidth: "450px"
+                alignItems: "center",
+                justifyContent: { lg: "flex-start", xs: "center" },
               }}
             >
-              <Box>
-                <ChatBubbleOutlineIcon
-                  sx={{
-                    fontSize: "4rem",
-                    backgroundColor: color,
-                    color: "white",
-                    padding: "10px",
-                    borderRadius: "10px",
-                  }}
-                />
+              <Box
+                sx={{
+                  textAlign: "center",
+                  padding: "20px",
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: "40px",
+                  maxWidth: "450px",
+                  alignItems: "center",
+                }}
+              >
+                <Box>
+                  <ChatBubbleOutlineIcon
+                    sx={{
+                      fontSize: "4rem",
+                      backgroundColor: color,
+                      color: "white",
+                      padding: "10px",
+                      borderRadius: "10px",
+                    }}
+                  />
+                  <Typography
+                    color={color}
+                    variant="h2"
+                    fontSize="2.5rem"
+                    fontWeight="bold"
+                  >
+                    Create Account
+                  </Typography>
+                  <Typography color="gray" variant="body2" fontSize="1.2rem">
+                    Get started with your free account
+                  </Typography>
+                </Box>
+                <form onSubmit={handleSubmit}>
+                  {/* Full Name */}
+                  <TextField
+                    label="Full Name"
+                    name="fullName"
+                    value={formData.fullName}
+                    onChange={(e) =>
+                      setFormData({ ...formData, fullName: e.target.value })
+                    }
+                    fullWidth
+                    margin="normal"
+                    required
+                  />
+
+                  {/* Email */}
+                  <TextField
+                    label="Email"
+                    name="email"
+                    type="email"
+                    value={formData.email}
+                    onChange={(e) =>
+                      setFormData({ ...formData, email: e.target.value })
+                    }
+                    fullWidth
+                    margin="normal"
+                    required
+                  />
+
+                  {/* Password */}
+                  <TextField
+                    label="Password"
+                    name="password"
+                    type={showPassword ? "text" : "password"}
+                    value={formData.password}
+                    onChange={(e) =>
+                      setFormData({ ...formData, password: e.target.value })
+                    }
+                    fullWidth
+                    margin="normal"
+                    required
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <IconButton
+                            onClick={() => setShowPassword(!showPassword)}
+                            edge="end"
+                            aria-label={
+                              showPassword ? "Hide password" : "Show password"
+                            }
+                          >
+                            {showPassword ? (
+                              <VisibilityOffIcon />
+                            ) : (
+                              <VisibilityIcon />
+                            )}
+                          </IconButton>
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+
+                  {/* Signup Button */}
+                  <Button
+                    sx={{
+                      backgroundColor: color,
+                      color: "white",
+                      border: "1px solid white",
+                      width: "200px",
+                      padding: "10px 20px",
+                      fontSize: "1.2rem",
+                      borderRadius: "10px",
+                      mt: "50px",
+                    }}
+                  >
+                    SIGN UP
+                  </Button>
+                </form>
+                <Box sx={{ display: { xs: "flex", md: "none" } }}>
+                  <Stack direction="row" gap="10px">
+                    <Typography variant="span">
+                      Don't have an account?
+                    </Typography>
+                    <Link to="/login">LogIn</Link>
+                  </Stack>
+                </Box>
+              </Box>
+            </Box>
+          </Grid>
+          {/* Other Page Link Side */}
+          <Grid item xs={12} md={6}>
+            <Box
+              sx={{
+                backgroundColor: color,
+                height: "100%",
+                borderRadius: "5% 0 0 5%",
+                display: { md: "flex", xs: "none" },
+                justifyContent: "flex-end",
+              }}
+            >
+              <Box
+                sx={{
+                  color: "white",
+                  padding: "70px",
+                  textAlign: "center",
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  height: "100%",
+                  maxWidth: "550px",
+                }}
+              >
                 <Typography
-                  color={color}
                   variant="h2"
+                  textAlign="center"
                   fontSize="2.5rem"
                   fontWeight="bold"
                 >
-                  Create Account
+                  Already have an account?
                 </Typography>
-                <Typography variant="body2" fontSize="1.2rem">
-                  Get started with your free account
+                <Typography
+                  color="black"
+                  marginTop="50px"
+                  variant="body2"
+                  fontSize="1.2rem"
+                >
+                  Login to stay connected to your friends
                 </Typography>
-              </Box>
-              <form onSubmit={handleSubmit}>
-                {/* Full Name */}
-                <TextField
-                  label="Full Name"
-                  name="fullName"
-                  value={formData.fullName}
-                  onChange={(e) =>
-                    setFormData({ ...formData, fullName: e.target.value })
-                  }
-                  fullWidth
-                  margin="normal"
-                  required
-                />
-
-                {/* Email */}
-                <TextField
-                  label="Email"
-                  name="email"
-                  type="email"
-                  value={formData.email}
-                  onChange={(e) =>
-                    setFormData({ ...formData, email: e.target.value })
-                  }
-                  fullWidth
-                  margin="normal"
-                  required
-                />
-
-                {/* Password */}
-                <TextField
-                  label="Password"
-                  name="password"
-                  type={showPassword ? "text" : "password"}
-                  value={formData.password}
-                  onChange={(e) =>
-                    setFormData({ ...formData, password: e.target.value })
-                  }
-                  fullWidth
-                  margin="normal"
-                  required
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <IconButton
-                          onClick={() => setShowPassword(!showPassword)}
-                          edge="end"
-                          aria-label={
-                            showPassword ? "Hide password" : "Show password"
-                          }
-                        >
-                          {showPassword ? (
-                            <VisibilityOffIcon />
-                          ) : (
-                            <VisibilityIcon />
-                          )}
-                        </IconButton>
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-
-                {/* Signup Button */}
                 <Button
+                  component={Link}
+                  to="/login"
                   sx={{
                     backgroundColor: color,
                     color: "white",
@@ -146,66 +240,13 @@ const SignUpPage = () => {
                     mt: "50px",
                   }}
                 >
-                  SIGN UP
+                  LOGIN
                 </Button>
-              </form>
+              </Box>
             </Box>
-          </Box>
+          </Grid>
         </Grid>
-        <Grid item xs={12} md={6}>
-          <Box
-            sx={{
-              backgroundColor: color,
-              height: "100%",
-              borderRadius: "5% 0 0 5%",
-              display: "flex",
-              justifyContent: "flex-end"
-            }}
-          >
-            <Box
-              sx={{
-                color: "white",
-                padding: "70px",
-                textAlign: "center",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-                height: "100%",
-                maxWidth: "550px",
-              }}
-            >
-              <Typography
-                variant="h2"
-                textAlign="center"
-                fontSize="2.5rem"
-                fontWeight="bold"
-              >
-                Already have an account?
-              </Typography>
-              <Typography marginTop="50px" variant="body2" fontSize="1.2rem">
-                Login to stay connected to your friends
-              </Typography>
-              <Button
-                component={Link}
-                to="/login"
-                sx={{
-                  backgroundColor: color,
-                  color: "white",
-                  border: "1px solid white",
-                  width: "200px",
-                  padding: "10px 20px",
-                  fontSize: "1.2rem",
-                  borderRadius: "10px",
-                  mt: "50px",
-                }}
-              >
-                LOGIN
-              </Button>
-            </Box>
-          </Box>
-        </Grid>
-      </Grid>
+      </Box>
     </Box>
   );
 };
